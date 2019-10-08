f46339c38e08d04ef294eeb25f8bd329b2d57ae6
gautierlef <gautier.leffebvre.sio@gmail.com>
2019-10-08 16:10:44 +0200

    AJout du constructeur de la classe Repas

--- awajaba/AwajabaNB06/src/awajabanb06/com/awa/awajaba/Repas.java
+++ awajaba/AwajabaNB06/src/awajabanb06/com/awa/awajaba/Repas.java
@@ -14,4 +14,9 @@ import java.time.LocalDate;
 public class Repas {
     private int numero;
     private LocalDate date;
+
+    public Repas(int numero, LocalDate date) {
+        this.numero = numero;
+        this.date = date;
+    }
 }
